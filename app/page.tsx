1c1950eacebaaafb41c41104985c5f9fecc4c127
Yusuf Felekoglu <yusuf.felekoglu@venhancer.com>
2025-08-26 17:18:33 +0300

    feat: feat #2

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
             </code>
             fix #1
             feat #1
+            feat #2
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
