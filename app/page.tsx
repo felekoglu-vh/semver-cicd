b0218e38b13cf8644c563809aad4e05484a1ebc8
Yusuf Felekoglu <yusuf.felekoglu@venhancer.com>
2025-08-26 16:49:36 +0300

    fix: address issue #1 in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
             test fix
             test feat
             test fix
+            fix #1
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
