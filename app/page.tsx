3d7fe65adc73997bb30ccd821480218065333e39
Yusuf Felekoglu <yusuf.felekoglu@venhancer.com>
2025-08-26 14:15:59 +0300

    feat: test

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
+            ver 1.0.0
           </li>
         </ol>
 
